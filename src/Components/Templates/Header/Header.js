e59a9316161ee47f05d8966494febfedaf60d274
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2023-12-05 00:31:33 +0330

    create Header section

---
+++ src/Components/Templates/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import HeaderSlider from '../../Modules/HeaderSlider/HeaderSlider'
+
+export default function Header() {
+    return (
+        <div className=' mt-5 '>
+            <HeaderSlider />
+        </div>
+    )
+}
