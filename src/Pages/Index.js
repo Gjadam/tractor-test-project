d02ad9b60519ebdba977cad42354bdc08e1cbf2a
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2023-12-05 00:32:23 +0330

    create Index Page

---
+++ src/Pages/Index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import TopBar from '../Components/Templates/TopBar/TopBar'
+import Header from '../Components/Templates/Header/Header'
+import Articles from '../Components/Templates/Articles/Articles'
+
+export default function Index() {
+    return (
+        <>
+            <TopBar />
+            <Header />
+            <Articles />
+        </>
+    )
+}
