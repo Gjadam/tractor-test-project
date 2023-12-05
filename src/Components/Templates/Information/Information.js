678f47d93445652a84f800c7fa643ebf4ba555a7
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2023-12-05 22:53:32 +0330

    create information section slider

--- src/Components/Templates/Information/Information.js
+++ src/Components/Templates/Information/Information.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import './Information.css'
 import { Container } from 'react-bootstrap'
+import { Autoplay, Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import InformationBox from '../../Modules/InformationBox/InformationBox'
+import InformationSlide from '../../Modules/InformationSlide/InformationSlide';
 export default function Information() {
     return (
         <div className='information my-5 '>
@@ -20,6 +23,62 @@ export default function Information() {
                     <div className="col-12 col-md-6 col-lg-3">
                         <InformationBox imgUrl={"/images/png/information-icon-4.png"} count={"4600"} title={"خرده فروشی های تجاری"} text={"شبکه خرده فروشی های ما می تواند به همه جای دنیا دسترسی داشته باشد."} />
                     </div>
+                </div>
+                <div className="">
+                    <Swiper
+                        slidesPerView={2}
+                        spaceBetween={5}
+                        loop
+                        autoplay={{
+                            delay: 2500,
+                            disableOnInteraction: false,
+                        }}
+                        breakpoints={{
+                            576: {
+                                slidesPerView: 2,
+                            },
+                            768: {
+                                slidesPerView: 3,
+                            },
+                            992: {
+                                slidesPerView: 4,
+                            },
+                            1200: {
+                                slidesPerView: 5,
+                            },
+                        }}
+                        modules={[Autoplay, Pagination]}
+                        className="mySwiper"
+                    >
+                        <SwiperSlide>
+                            <InformationSlide imgUrl={"/images/png/information-logo-1.png"} imgHoverUrl={"/images/png/information-logo-hover-1.png"} />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <InformationSlide imgUrl={"/images/png/information-logo-2.png"} imgHoverUrl={"/images/png/information-logo-hover-2.png"} />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <InformationSlide imgUrl={"/images/png/information-logo-3.png"} imgHoverUrl={"/images/png/information-logo-hover-3.png"} />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <InformationSlide imgUrl={"/images/png/information-logo-4.png"} imgHoverUrl={"/images/png/information-logo-hover-4.png"} />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <InformationSlide imgUrl={"/images/png/information-logo-5.png"} imgHoverUrl={"/images/png/information-logo-hover-5.png"} />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <InformationSlide imgUrl={"/images/png/information-logo-6.png"} imgHoverUrl={"/images/png/information-logo-hover-6.png"} />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <InformationSlide imgUrl={"/images/png/information-logo-7.png"} imgHoverUrl={"/images/png/information-logo-hover-7.png"} />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <InformationSlide imgUrl={"/images/png/information-logo-8.png"} imgHoverUrl={"/images/png/information-logo-hover-8.png"} />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <InformationSlide imgUrl={"/images/png/information-logo-9.png"} imgHoverUrl={"/images/png/information-logo-hover-9.png"} />
+                        </SwiperSlide>
+
+                    </Swiper>
                 </div>
             </Container>
         </div>
