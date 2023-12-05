5130e6df7feb7356ab6b024a28eae41d6051daaf
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2023-12-05 22:52:24 +0330

    add  articles-box style

--- src/Components/Templates/Articles/Articles.js
+++ src/Components/Templates/Articles/Articles.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './Articles.css'
 import ArticleBox from '../../Modules/ArticleBox/ArticleBox'
 import { Container } from 'react-bootstrap'
 
@@ -6,16 +7,16 @@ export default function Articles() {
     return (
         <Container>
             <div className=" w-100 row my-5 ">
-                <div className=" col-12 col-md-3  ">
+                <div className="articles-box col-12 col-md-3  ">
                     <ArticleBox imgUrl={'/images/png/article-icon-1.png'} title={"خودروسازی"} text={"مهندسی صنایع با تولید سروکار ندارد."} />
                 </div>
-                <div className=" col-12 col-md-3  ">
+                <div className="articles-box col-12 col-md-3  ">
                     <ArticleBox imgUrl={'/images/png/article-icon-2.png'} title={"نوآوری های مجدد صنعتی"} text={"نمای چشم انداز را طراحی می کنیم و تجهیزات پایه را نصب می کنیم."} />
                 </div>
-                <div className=" col-12 col-md-3  ">
+                <div className="articles-box col-12 col-md-3  ">
                     <ArticleBox imgUrl={'/images/png/article-icon-3.png'} title={"صنعت تحقیقات شیمی"} text={"برای کمک به تولید مواد غذایی و میکرو فیبر توسعه یافته است."} />
                 </div>
-                <div className=" col-12 col-md-3  ">
+                <div className="articles-box col-12 col-md-3  ">
                     <ArticleBox imgUrl={'/images/png/article-icon-4.png'} title={"سازه های مکانیکی"} text={"ساخت پل معمولاً شامل استفاده از روش های گسسته است."} />
                 </div>
             </div>
