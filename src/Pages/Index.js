8203757b6407621f952b0d2e428b1c84956f6bf2
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2023-12-05 04:30:01 +0330

    add sections in Index page

--- src/Pages/Index.js
+++ src/Pages/Index.js
@@ -2,6 +2,8 @@ import React from 'react'
 import TopBar from '../Components/Templates/TopBar/TopBar'
 import Header from '../Components/Templates/Header/Header'
 import Articles from '../Components/Templates/Articles/Articles'
+import About from '../Components/Templates/About/About'
+import Information from '../Components/Templates/Information/Information'
 
 export default function Index() {
     return (
@@ -9,6 +11,8 @@ export default function Index() {
             <TopBar />
             <Header />
             <Articles />
+            <About />
+            <Information />
         </>
     )
 }
