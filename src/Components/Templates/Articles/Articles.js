7b857b91e23868b85456bd2093aea174c4ee2b82
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2023-12-05 04:29:05 +0330

    change some style

--- src/Components/Templates/Articles/Articles.js
+++ src/Components/Templates/Articles/Articles.js
@@ -5,11 +5,19 @@ import { Container } from 'react-bootstrap'
 export default function Articles() {
     return (
         <Container>
-            <div className=" d-flex justify-content-center align-items-center flex-wrap my-5 gap-5">
-                <ArticleBox imgUrl={'/images/png/article-icon-1.png'} title={"خودروسازی"} text={"مهندسی صنایع با تولید سروکار ندارد."} />
-                <ArticleBox imgUrl={'/images/png/article-icon-2.png'} title={"نوآوری های مجدد صنعتی"} text={"نمای چشم انداز را طراحی می کنیم و تجهیزات پایه را نصب می کنیم."} />
-                <ArticleBox imgUrl={'/images/png/article-icon-3.png'} title={"صنعت تحقیقات شیمی"} text={"برای کمک به تولید مواد غذایی و میکرو فیبر توسعه یافته است."} />
-                <ArticleBox imgUrl={'/images/png/article-icon-4.png'} title={"سازه های مکانیکی"} text={"ساخت پل معمولاً شامل استفاده از روش های گسسته است."} />
+            <div className=" w-100 row my-5 ">
+                <div className=" col-12 col-md-3  ">
+                    <ArticleBox imgUrl={'/images/png/article-icon-1.png'} title={"خودروسازی"} text={"مهندسی صنایع با تولید سروکار ندارد."} />
+                </div>
+                <div className=" col-12 col-md-3  ">
+                    <ArticleBox imgUrl={'/images/png/article-icon-2.png'} title={"نوآوری های مجدد صنعتی"} text={"نمای چشم انداز را طراحی می کنیم و تجهیزات پایه را نصب می کنیم."} />
+                </div>
+                <div className=" col-12 col-md-3  ">
+                    <ArticleBox imgUrl={'/images/png/article-icon-3.png'} title={"صنعت تحقیقات شیمی"} text={"برای کمک به تولید مواد غذایی و میکرو فیبر توسعه یافته است."} />
+                </div>
+                <div className=" col-12 col-md-3  ">
+                    <ArticleBox imgUrl={'/images/png/article-icon-4.png'} title={"سازه های مکانیکی"} text={"ساخت پل معمولاً شامل استفاده از روش های گسسته است."} />
+                </div>
             </div>
         </Container>
     )
