0d27dc35f6d442b2fb9479011a8787c6e6d04c6c
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2023-12-05 04:27:50 +0330

    change articleBox Styles

--- src/Components/Modules/ArticleBox/ArticleBox.js
+++ src/Components/Modules/ArticleBox/ArticleBox.js
@@ -6,12 +6,12 @@ export default function ArticleBox({ imgUrl, title, text }) {
     return (
         <>
             <a href="" className='article-box text-decoration-none '>
-                <div className=' d-flex justify-content-between  align-items-center flex-column row-gap-2 '>
-                    <div className="article-box__icon mb-3 ">
-                        <img src={imgUrl} className='' alt="icon" />
+                <div className=' d-flex justify-content-between  align-items-center flex-column row-gap-2 p-3 border-0 '>
+                    <div className="article-box__icon__wrapper mb-3 ">
+                        <img src={imgUrl} className='article-box__icon' alt="icon" />
                     </div>
                     <div className="article-box__content d-flex justify-content-between align-items-center flex-column">
-                        <span className='article-box__title  fw-bold fs-4  '>{title}</span>
+                        <span className='article-box__title  fw-bold fs-4  text-center '>{title}</span>
                         <span className='article-box__text text-center  fs-6 mt-3 '>{text}</span>
                     </div>
                     <div className='article-box__link'>
