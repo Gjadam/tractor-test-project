8449669bb49a488d829b370d29bf5c01503c79b9
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2023-12-06 03:33:59 +0330

    .

--- src/Components/Templates/About/About.js
+++ src/Components/Templates/About/About.js
@@ -7,7 +7,7 @@ export default function About() {
         <div className='about d-flex justify-content-between align-items-center w-100  my-5'>
             <div className=" about__right-bg h-100 w-100 d-lg-flex d-none  "></div>
             <div className="about__left-bg d-flex justify-content-around align-items-start flex-column h-100 w-100 p-5">
-                <h1 className='about__title fw-bold lh-base '>شرکتی که در زمینه خدمات و نگهداری فعالیت دارد.</h1>
+                <h2 className='about__title fw-bold lh-base '>شرکتی که در زمینه خدمات و نگهداری فعالیت دارد.</h2>
                 <div className=" d-flex ">
                     <AboutBox title={"مزایا برای شرکا"} text={"استراتژی های بقا برد-برد را برای اطمینان از تسلط فعالانه به میز بیاورید. در پایان روز، استراتژی‌های بقا برد-برد را به جدول منتقل کنید"} />
                     <AboutBox title={"مشاوره حرفه ای"} text={"مهندسی صنایع شاخه‌ای از مهندسی است که به بهینه‌سازی فرآیندها، سیستم‌ها یا سازمان‌های پیچیده می‌پردازد."} />
