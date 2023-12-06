8395033b2ad1bdc3d5b499dc86ffd8de927c697e
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2023-12-06 03:33:50 +0330

    create NewsBox component

---
+++ src/Components/Modules/NewsBox/NewsBox.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './NewsBox.css'
+export default function NewsBox({ imgUrl, category, title }) {
+    return (
+        <div className="news__boxes__box ">
+            <div className="news__boxes__box__image__wrapper">
+                <img src={imgUrl} className='news__boxes__box__image w-100 h-100 ' alt="image" />
+            </div>
+            <div className="news__boxes__box__content h-100  d-flex justify-content-end align-items-start flex-column text-end p-3 ">
+                <div className=" d-flex ">
+                    <span className='news__boxes__box__category py-0 px-2 mx-1 '>{category}</span>
+                </div>
+                <a href="" className=' text-decoration-none my-4'>
+                    <span className='news__boxes__box__title text-white fw-bold  fs-5 '>{title}</span>
+                </a>
+                <span className='news__boxes__box__date text-white fw-bold '>4 مرداد ، 1396</span>
+            </div>
+        </div>
+    )
+}
