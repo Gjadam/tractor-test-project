2d7ab12046c77c6a3287d81658c032c8c44fbd36
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2023-12-06 19:28:51 +0330

    create FooterList

---
+++ src/Components/Modules/FooterList/FooterList.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './FooterList.css'
+export default function FooterList({title, link1, link2, link3, link4, link5, link6}) {
+    return (
+        <ul className='footer__list__items'>
+            <h2 className='footer__list__title text-white fs-5 fw-bold '>{title}</h2>
+            <li className='footer__list'>
+                <a href="" className='footer__list__link link-offset-2'>{link1}</a>
+            </li>
+            <li className='footer__list'>
+                <a href="" className='footer__list__link link-offset-2'>{link2}</a>
+            </li>
+            <li className='footer__list'>
+                <a href="" className='footer__list__link link-offset-2'>{link3}</a>
+            </li>
+            <li className='footer__list'>
+                <a href="" className='footer__list__link link-offset-2'>{link4}</a>
+            </li>
+            <li className='footer__list'>
+                <a href="" className='footer__list__link link-offset-2'>{link5}</a>
+            </li>
+            <li className='footer__list'>
+                <a href="" className='footer__list__link link-offset-2'>{link6}</a>
+            </li>
+        </ul>
+    )
+}
