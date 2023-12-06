415636c9bbf9fcf9959ec3f4a241cf6a934dc035
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2023-12-06 19:29:00 +0330

    .

--- src/Components/Templates/ContactUs/ContactUs.js
+++ src/Components/Templates/ContactUs/ContactUs.js
@@ -40,7 +40,7 @@ export default function ContactUs() {
                         </form>
                     </div>
                 <div className="contact-us__map">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d193595.2527999406!2d-74.14448771969894!3d40.697631233373556!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2snl!4v1701858347713!5m2!1sen!2snl" width="100%" height="850" style={{ border: "0" }} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d193595.2527999406!2d-74.14448771969894!3d40.697631233373556!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2s!4v1701865882514!5m2!1sen!2s" width="100%" height="850" style={{border: "0"}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
             </div>
         </>
