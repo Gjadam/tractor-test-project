36a1b23e2f040942d7dd9181bbcb30080aa6ccac
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2023-12-07 14:03:16 +0330

    FooterList change styles

--- src/Components/Modules/FooterList/FooterList.js
+++ src/Components/Modules/FooterList/FooterList.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import './FooterList.css'
-export default function FooterList({title, link1, link2, link3, link4, link5, link6}) {
+export default function FooterList({ title, link1, link2, link3, link4, link5, link6 }) {
     return (
         <ul className='footer__list__items'>
-            <h2 className='footer__list__title text-white fs-5 fw-bold '>{title}</h2>
+            <h2 className='footer__list__title text-white fs-6 fw-bold '>{title}</h2>
             <li className='footer__list'>
                 <a href="" className='footer__list__link link-offset-2'>{link1}</a>
             </li>
