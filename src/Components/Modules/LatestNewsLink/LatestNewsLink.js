96cd588c55079977652d07d54c90214ae11d2277
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2023-12-08 00:55:32 +0330

    text bolder

--- src/Components/Modules/LatestNewsLink/LatestNewsLink.js
+++ src/Components/Modules/LatestNewsLink/LatestNewsLink.js
@@ -4,7 +4,7 @@ export default function LatestNewsLink({title}) {
     return (
         <div className="news__latest-news__box border-bottom py-3 ">
             <a href="" className='news__latest-news__box__title__link link-offset-2'>
-                <h4 className='news__latest-news__box__title fw-bold lh-base '>{title}</h4>
+                <h5 className='news__latest-news__box__title fw-bolder  lh-base '>{title}</h5>
             </a>
             <h6 className='news__latest-news__box__date fw-bold mt-3 '>18 مرداد ، 1396</h6>
         </div>
