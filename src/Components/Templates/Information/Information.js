142fc33dae1809242696a9e4f8dfab532147cfe1
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2023-12-08 00:57:05 +0330

    .

--- src/Components/Templates/Information/Information.js
+++ src/Components/Templates/Information/Information.js
@@ -12,16 +12,16 @@ export default function Information() {
                 <h2 className="information__title text-center fw-bold "  data-aos="fade-up" data-aos-duration="800">اعداد برای خودشان صحبت می کنند</h2>
                 <div className=" w-100  row "  data-aos="fade-up" data-aos-duration="900">
                     <div className="col-12 col-md-6 col-lg-3">
-                        <InformationBox imgUrl={"/images/png/information-icon-1.png"} count={"2000"} title={"شعب سراسر کشور"} text={"شعب ما در سراسر کشور و خارج از کشور گسترش یافته است."} />
+                        <InformationBox imgUrl={"/images/png/information-icon-1.png"} count={2000} title={"شعب سراسر کشور"} text={"شعب ما در سراسر کشور و خارج از کشور گسترش یافته است."} />
                     </div>
                     <div className="col-12 col-md-6 col-lg-3">
-                        <InformationBox imgUrl={"/images/png/information-icon-2.png"} count={"3000"} title={"پروژه های انجام شده"} text={"ما به تعداد عظیمی از پروژه های تکمیل شده افتخار می کنیم."} />
+                        <InformationBox imgUrl={"/images/png/information-icon-2.png"} count={3000} title={"پروژه های انجام شده"} text={"ما به تعداد عظیمی از پروژه های تکمیل شده افتخار می کنیم."} />
                     </div>
                     <div className="col-12 col-md-6 col-lg-3">
-                        <InformationBox imgUrl={"/images/png/information-icon-3.png"} count={"543"} title={"درختان کاشته شده"} text={"کاشت درختان سبز برای حفظ پایداری"} />
+                        <InformationBox imgUrl={"/images/png/information-icon-3.png"} count={543} title={"درختان کاشته شده"} text={"کاشت درختان سبز برای حفظ پایداری"} />
                     </div>
                     <div className="col-12 col-md-6 col-lg-3">
-                        <InformationBox imgUrl={"/images/png/information-icon-4.png"} count={"4600"} title={"خرده فروشی های تجاری"} text={"شبکه خرده فروشی های ما می تواند به همه جای دنیا دسترسی داشته باشد."} />
+                        <InformationBox imgUrl={"/images/png/information-icon-4.png"} count={4600} title={"خرده فروشی های تجاری"} text={"شبکه خرده فروشی های ما می تواند به همه جای دنیا دسترسی داشته باشد."} />
                     </div>
                 </div>
                 <div className=""  data-aos="fade-up" data-aos-duration="1000">
