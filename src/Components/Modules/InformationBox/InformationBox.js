dfc32813dd12f4841c18e177e84d92bdde0feacf
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2023-12-08 00:55:16 +0330

    add CountUp

--- src/Components/Modules/InformationBox/InformationBox.js
+++ src/Components/Modules/InformationBox/InformationBox.js
@@ -1,6 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './InformationBox.css'
 export default function InformationBox({ imgUrl, count, title, text }) {
+
+    const [counter, setCounter] = useState(0)
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCounter(prevCount => prevCount + 1)
+        }, 0.1);
+
+        if (counter === count) {
+            clearInterval(interval)
+        }
+
+        return () => clearInterval(interval)
+
+    }, [counter])
+
     return (
         <div className='d-flex justify-content-between align-items-start flex-column my-5 w-100 '>
             <div className=" d-flex justify-content-center align-items-start flex-column text-end w-100 ">
@@ -8,7 +24,7 @@ export default function InformationBox({ imgUrl, count, title, text }) {
                     <img src={imgUrl} alt="icon" />
                 </div>
                 <div className=" d-flex justify-content-center align-items-center ">
-                    <span className='information-box__count fw-bold'>{count}</span>
+                    <span className='information-box__count fw-bold'>{counter}</span>
                 </div>
                 <div className=" d-flex justify-content-center align-items-start flex-column w-100  ">
                     <span className='information-box__title fw-bold mb-3'>{title}</span>
