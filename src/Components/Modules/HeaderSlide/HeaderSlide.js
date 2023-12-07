8a7eac9604e93a57e1334e99e934e6df33f7dd4c
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2023-12-07 21:16:51 +0330

    change Header styles

--- src/Components/Modules/HeaderSlide/HeaderSlide.js
+++ src/Components/Modules/HeaderSlide/HeaderSlide.js
@@ -6,16 +6,16 @@ export default function HeaderSlide({ imgUrl, text, title, boxButtonTitle, norma
     return (
         <div className="header-slider d-flex justify-content-center align-items-center ">
             <img src={imgUrl} className='header-slider__image' alt="image" />
-            <div className={`header-slider__content-wrapper d-flex ${slide === 3 ? "align-items-start" : "align-items-center"} justify-content-center flex-column text-white `}>
-                <div className={slide === 3 && " d-flex  flex-column-reverse "}>
-                    <h5 className={`fw-bold ${slide === 3 && "text-end"}  ${slide === 2 && "text-primary "}`}>{text}</h5>
-                    <h1 className={`header-slider__title my-4 fw-bolder `}>{title}</h1>
+            <div className={`header-slider__content-wrapper p-3 d-flex ${slide === 3 ? "align-items-start" : "align-items-center"} justify-content-center flex-column text-white `}>
+                <div className={`header-slider__content__texts  ${slide === 3 && " d-flex  flex-column-reverse "}`}>
+                    <h5 className={`header-slider__text fw-bold ${slide === 3 && " text-end  align-self-start "}  ${slide === 2 && "text-primary "}`}  data-aos="fade-down" data-aos-duration="1500">{text}</h5>
+                    <h1 className={`header-slider__title my-4 fw-bolder `}  data-aos="fade-down" data-aos-duration="1000">{title}</h1>
                 </div>
                 <div className=" d-flex justify-content-center align-items-center mt-4  column-gap-3">
-                    <Button text={boxButtonTitle} />
+                    <Button text={boxButtonTitle}  />
                     {
                         normalButtonTitle.length > 0 &&
-                        <a href="" className={`${slide === 3 && "d-none"} text-decoration-none text-white`}>
+                        <a href="" className={`${slide === 3 && "d-none"} text-decoration-none text-white` } data-aos="fade-right" data-aos-duration="1000">
                             <TiMediaPlayReverse className='header-slider__icon-color ms-1 ' />
                             <span className='header-slider__btn-hover'>{normalButtonTitle}</span>
                         </a>
